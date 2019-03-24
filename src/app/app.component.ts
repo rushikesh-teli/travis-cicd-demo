d5ab80eb68daff6c9f533e8f395bcd20c28cfeb9
mxcheung <maxcheung@optusnet.com.au>
2019-03-24 19:02:05 +1100

    tidy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Show, tvmazemxcheungService } from 'tvmazemxcheung';
 })
 export class AppComponent {
   show$: Observable<Show>;
-  showId = 341;
+  showId = 342;
 
   constructor(private tvmazemxcheung: tvmazemxcheungService) {
     this.show$ = this.tvmazemxcheung.getShow(this.showId);
