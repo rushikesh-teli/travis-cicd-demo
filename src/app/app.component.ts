f9bf4d450ad6b608399cf5d0cb9883809a6dec4e
mxcheung <maxcheung@optusnet.com.au>
2019-03-26 06:03:41 +1100

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { FootermxcheungComponent } from 'footermxcheung';
 })
 export class AppComponent {
   show$: Observable<Show>;
-  showId = 354;
+  showId = 355;
 
   constructor(private tvmazemxcheung: tvmazemxcheungService) {
     this.show$ = this.tvmazemxcheung.getShow(this.showId);
